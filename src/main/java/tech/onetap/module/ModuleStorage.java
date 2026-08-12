27f328bae64268b655e98ac4979b0ffe45438b8f
plsmeow <fireping2011@gmail.com>
2026-08-12 12:29:51 +0500

    AntiAim

--- src/main/java/tech/onetap/module/ModuleStorage.java
+++ src/main/java/tech/onetap/module/ModuleStorage.java
@@ -36,7 +36,7 @@ public class ModuleStorage implements IMinecraft {
         modules.addAll(List.of(
                 new FullBright(), new ClickGui(), new Optimization(), new Sprint(), new Troll(), new AiRecord(),
                   new TriggerBot(), new Criticals(), new MaceKill(), new AutoMace(), new BoatAura(), new TpAura(),
-                new NoRender(), new KillAura(), new AutoFlyMace(),
+                new NoRender(), new KillAura(), new KBDisplacement(), new AutoFlyMace(), new AntiAim(),
                 new Tags(), new TargetESP(), new NoPush(), new SoulESP(), new DragonFly(),
                 new NoJumpDelay(), new TeleportBack(), new ElytraHelper(), new HotbarRefill(), new Flight(),
                 new AutoTotem(), new ClickPearl(), new ClickTP(), new UseTracker(), new CrystalAura(), new AnchorAura(), new Scaffold(),
